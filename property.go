4b96d6180a03401fdb892601bf73d21178da097c
Henry Tseng <henrybear327@gmail.com>
2019-11-12 09:07:49 +0800

    update Text()

--- property.go
+++ property.go
@@ -67,7 +67,7 @@ func (s *Selection) Text() string {
 	f = func(n *html.Node) {
 		if n.Type == html.TextNode {
 			// Keep newlines and spaces, like jQuery
-			buf.WriteString(n.Data)
+			buf.WriteString(n.Data + " ")
 		}
 		if n.FirstChild != nil {
 			for c := n.FirstChild; c != nil; c = c.NextSibling {
